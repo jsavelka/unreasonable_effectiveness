1627b0416ea9bba67030a61c61c1d0ff5aa01c70
jsavelka <j.savelka@gmail.com>
2024-01-22 14:47:10 -0500

    Update user_message.py

--- user_message.py
+++ user_message.py
@@ -1,5 +1,15 @@
-def generate_user_message(texts, num_start=1, text_type='Sentence'):
-    return '\n'.join(
-        f'{text_type} {i}: {text}\n' 
+EXPECTED_OUTPUT_STR = """EXPECTED OUTPUT
+Return only the labels for the snippets above. Do not return any 
+other text. Use the following format:
+
+Snippet 1: <LABEL>
+Snippet 2: <LABEL>
+Snippet 3: <LABEL>
+..."""
+
+
+def generate_user_message(texts, num_start=1):
+    return 'SENTENCES\n' + '\n'.join(
+        f'Snippet {i}: {text}\n' 
         for i, text in enumerate(texts, start=num_start)
-    )
+    ) + '\n\n' + EXPECTED_OUTPUT_STR
